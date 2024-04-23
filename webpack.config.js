06eacdcd10c449dfdfccd2645955b32281a87e3d
NaJaeWan <2015132011@kpu.ac.kr>
2024-04-23 23:24:46 +0900

    chore : babel & webpack 설정

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./app.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+
+  devServer: {
+    compress: true,
+    port: 9999,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "React 만들기",
+      template: "index.html",
+    }),
+  ],
+};
